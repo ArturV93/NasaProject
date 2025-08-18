cc2526c487f18ca41ddf0b48f8ddc8fc27551586
Artur <arturv1404@gmail.com>
2025-08-18 22:20:43 +0100

    chore: Added the default url

--- frontend/src/lib/axiosClient.ts
+++ frontend/src/lib/axiosClient.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
 
 // Create a singleton Axios instance
 const axiosClient: AxiosInstance = axios.create({
-  baseURL: import.meta.env["API_BASE_URL"] || "http://localhost:3030",
+  baseURL: import.meta.env["API_BASE_URL"] || "https://backend-snowy-silence-3777.fly.dev",
   headers: {
     "Content-Type": "application/json",
   },
